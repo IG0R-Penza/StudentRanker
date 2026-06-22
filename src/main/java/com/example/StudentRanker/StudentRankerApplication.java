f26f9e50841830fcc340871708bfe8566adf0618
IG0R-Penza <igorkovalev2006@gmail.com>
2026-06-22 20:17:00 +0300

    zero commit

---
+++ src/main/java/com/example/StudentRanker/StudentRankerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentRanker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRankerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRankerApplication.class, args);
+    }
+
+}
